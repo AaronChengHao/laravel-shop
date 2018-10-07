482fcd79e30cf8f60325c23615e7c7726764185e
chenghao <616915054@qq.com>
2018-10-07 12:17:58 +0800

    后台众筹商品管理

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -35,4 +35,11 @@ Route::group([
     $router->delete('categories/{id}', 'CategoriesController@destroy');
     $router->get('api/categories', 'CategoriesController@apiIndex');
 
+
+    $router->get('crowdfunding_products', 'CrowdfundingProductsController@index');
+    $router->get('crowdfunding_products/create', 'CrowdfundingProductsController@create');
+    $router->post('crowdfunding_products', 'CrowdfundingProductsController@store');
+    $router->get('crowdfunding_products/{id}/edit', 'CrowdfundingProductsController@edit');
+    $router->put('crowdfunding_products/{id}', 'CrowdfundingProductsController@update');
+
 });
