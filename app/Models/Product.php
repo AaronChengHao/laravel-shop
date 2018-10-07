8d7e675d05bad9ca626d07def459e24ba63e13b4
chenghao <616915054@qq.com>
2018-10-07 11:08:51 +0800

    初始化众筹模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,10 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['title', 'description', 'image', 'on_sale', 'rating', 'sold_count', 'review_count', 'price'];
+
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+
+    public static $typeMap = [
+        self::TYPE_NORMAL => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品'
+    ];
+
+    protected $fillable = [
+        'title',
+        'description',
+        'image', 'on_sale',
+        'rating',
+        'sold_count',
+        'review_count',
+        'price',
+        'type'
+    ];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
+
+    public function crowdfunding()
+    {
+        return $this->hasOne(crowdfundingProduct::class);
+    }
+
     // 与商品SKU关联
     public function skus()
     {
