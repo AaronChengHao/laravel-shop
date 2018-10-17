00d3e3a12ea7be21db183cf921d631b903b3c7da
chenghao <616915054@qq.com>
2018-10-17 23:25:21 +0800

    分期付款退款

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -16,6 +16,7 @@ class VerifyCsrfToken extends Middleware
         'payment/wechat/notify',
         'payment/wechat/refund_notify',
         'installments/alipay/notify',
-        'installments/wechat/notify'
+        'installments/wechat/notify',
+        'installments/wechat/refund_notify'
     ];
 }
